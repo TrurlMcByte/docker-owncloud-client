8e688a4e687ddeea8cc56fe3931f8a4309f5a4cf
Trurl McByte <trurl@mcbyte.net>
2016-06-21 12:36:35 +0300

    fix miss exclude

--- startup.sh
+++ startup.sh
@@ -7,6 +7,7 @@ WORK_GID=${WORK_GID:-82}
 
 INTERVAL=${INTERVAL:-30}
 WORK_GROUP=clouddata
+LOCALDIR=${LOCALDIR:-/data}
 
 addgroup -S -g $WORK_GID $WORK_GROUP || WORK_GROUP=`awk -F: -v g=8 '$3==g {print $1}' /etc/group`
 adduser -u $WORK_UID -D -s /bin/sh -S -G $WORK_GROUP clouddata
@@ -18,9 +19,15 @@ if [ "$USER" -a  "$PASSWORD" ] ; then
     echo "	password $PASSWORD" >> /home/clouddata/.netrc
 fi
 
+mkdir -p /etc/owncloud-client
+touch /etc/owncloud-client/sync-exclude.lst
+mkdir -p /etc/ownCloud
+touch /etc/ownCloud/sync-exclude.lst
+
 mkdir -p /home/clouddata/.local/share/data/ownCloud
 touch /home/clouddata/.local/share/data/ownCloud/cookies.db
 
+touch $LOCALDIR/exclude.lst
 chown $WORK_UID.$WORK_GID /home/clouddata/.netrc
 chown -R $WORK_UID.$WORK_GID /home/clouddata/.local
 chown -R $WORK_UID.$WORK_GID $LOCALDIR
@@ -28,6 +35,6 @@ chown -R $WORK_UID.$WORK_GID $LOCALDIR
 while true
 do
     # Start sync
-    su clouddata -c "owncloudcmd --trust --non-interactive --silent -n $LOCALDIR $URL"
+    su clouddata -c "owncloudcmd --trust --non-interactive --silent -h -n $LOCALDIR $URL &> /home/clouddata/sync.log"
     sleep $INTERVAL
 done
